82dd10e419ff495657abc21012894406d526765b
Federico Dionisi <federicodionisi@think-it.io>
2023-02-16 17:10:46 -0100

    feat!: rename data to management and ids to protocol (#15)

--- tests/controllers/public.test.ts
+++ tests/controllers/public.test.ts
@@ -12,15 +12,15 @@ describe("PublicController", () => {
   const apiToken = "123456";
   const consumer: Addresses = {
     default: "http://localhost:19191/api",
-    data: "http://localhost:19193/api/v1/data",
-    ids: "http://consumer-connector:9194/api/v1/ids",
+    management: "http://localhost:19193/api/v1/data",
+    protocol: "http://consumer-connector:9194/api/v1/ids",
     public: "http://localhost:19291/public",
     control: "http://localhost:19292/control",
   };
   const provider: Addresses = {
     default: "http://localhost:29191/api",
-    data: "http://localhost:29193/api/v1/data",
-    ids: "http://provider-connector:9194/api/v1/ids",
+    management: "http://localhost:29193/api/v1/data",
+    protocol: "http://provider-connector:9194/api/v1/ids",
     public: "http://localhost:29291/public",
     control: "http://localhost:29292/control",
   };
@@ -78,7 +78,7 @@ describe("PublicController", () => {
         {
           assetId,
           "connectorId": "provider",
-          "connectorAddress": `${providerContext.ids}/data`,
+          "connectorAddress": `${providerContext.protocol}/data`,
           "contractId": contractAgreement.id,
           "managedResources": false,
           "dataDestination": { "type": "HttpProxy" },
