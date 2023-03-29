5a3477f87f83a4254d3605cd6f66f653721e47f8
AhmedGrati <ahmedgrati1999@gmail.com>
2023-03-29 18:11:42 +0100

    feat: add create asset feature
    Signed-off-by: AhmedGrati <ahmedgrati1999@gmail.com>

---
+++ go/service/assets/api_op_CreateAsset.go
@@ -0,0 +1,181 @@
+package assets
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+)
+
+type BaseAddress struct {
+	Type string
+}
+
+type HttpDataAddress struct {
+	*BaseAddress
+	Name             *string
+	Path             *string
+	Method           *string
+	BaseUrl          *string
+	AuthKey          *string
+	AuthCode         *string
+	SecretName       *string
+	ProxyBody        *string
+	ProxyPath        *string
+	ProxyQueryParams *string
+	ProxyMethod      *string
+	ContentType      *string
+}
+
+type S3StorageDataAddress struct {
+	*BaseAddress
+	Name            *string
+	BucketName      *string
+	AccessKeyId     *string
+	SecretAccessKey *string
+}
+
+type AzureStorageDataAddress struct {
+	*BaseAddress
+	Container *string
+	Account   *string
+	BlobName  *string
+}
+
+type DataAddress struct {
+	HttpDataAddress         *HttpDataAddress
+	S3StorageDataAddress    *S3StorageDataAddress
+	AzureStorageDataAddress *AzureStorageDataAddress
+}
+
+type AssetProperties map[string]string
+
+type Asset struct {
+	AssetProperties
+}
+
+type CreateAssetInput struct {
+	Asset
+	DataAddress
+}
+
+type CreateAssetOutput struct {
+	CreatedAt int64
+	Id        string
+}
+
+func (c *Client) CreateAsset(createAssetInput CreateAssetInput) (*CreateAssetOutput, error) {
+	endpoint := fmt.Sprintf("%v/assets", *c.Addresses.Management)
+	createAssetOutput := CreateAssetOutput{}
+
+	err := validateDataAddressInput(createAssetInput.DataAddress)
+	if err != nil {
+		return nil, err
+	}
+
+	dataAddressApiInput, err := createDataAddressFromInput(createAssetInput.DataAddress)
+	if err != nil {
+		return nil, err
+	}
+
+	createAssetApiInput := CreateAssetApiInput{
+		AssetApiInput:       createDataAssetFromInput(createAssetInput.Asset),
+		DataAddressApiInput: *dataAddressApiInput,
+	}
+
+	createAssetApiInputJson, err := json.Marshal(createAssetApiInput)
+
+	if err != nil {
+		return nil, fmt.Errorf("unexpected error while marshaling create asset input: %v", err)
+	}
+
+	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(createAssetApiInputJson))
+	if err != nil {
+		return nil, fmt.Errorf("unexpected error while building HTTP request: %v", err)
+	}
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error while performing POST request to the endpoint %v: %v", endpoint, err)
+	}
+
+	defer res.Body.Close()
+	response, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error while reading response body: %v", err)
+	}
+
+	err = json.Unmarshal(response, &createAssetOutput)
+	if err != nil {
+		return nil, fmt.Errorf("error while unmarshaling json: %v", err)
+	}
+
+	return &createAssetOutput, err
+}
+
+func createDataAddressFromInput(dataAddress DataAddress) (*DataAddressApiInput, error) {
+	if dataAddress.HttpDataAddress != nil {
+		// create http address
+		return &DataAddressApiInput{
+			DataProperties: HttpData{
+				Type:             "HttpData",
+				Name:             dataAddress.HttpDataAddress.Name,
+				Path:             dataAddress.HttpDataAddress.Path,
+				Method:           dataAddress.HttpDataAddress.Method,
+				BaseUrl:          dataAddress.HttpDataAddress.BaseUrl,
+				AuthKey:          dataAddress.HttpDataAddress.AuthKey,
+				AuthCode:         dataAddress.HttpDataAddress.AuthCode,
+				SecretName:       dataAddress.HttpDataAddress.SecretName,
+				ProxyPath:        dataAddress.HttpDataAddress.ProxyPath,
+				ProxyBody:        dataAddress.HttpDataAddress.ProxyBody,
+				ProxyQueryParams: dataAddress.HttpDataAddress.ProxyQueryParams,
+				ProxyMethod:      dataAddress.HttpDataAddress.ProxyMethod,
+				ContentType:      dataAddress.HttpDataAddress.ContentType,
+			},
+		}, nil
+	}
+	if dataAddress.AzureStorageDataAddress != nil {
+		return &DataAddressApiInput{
+			DataProperties: AzureData{
+				Type:      "AzureStorage",
+				Container: dataAddress.AzureStorageDataAddress.Container,
+				Account:   dataAddress.AzureStorageDataAddress.Account,
+				BlobName:  dataAddress.AzureStorageDataAddress.BlobName,
+			},
+		}, nil
+	}
+	if dataAddress.S3StorageDataAddress != nil {
+		return &DataAddressApiInput{
+			DataProperties: S3Data{
+				Type:            "AmazonS3",
+				Name:            dataAddress.S3StorageDataAddress.Name,
+				BucketName:      dataAddress.S3StorageDataAddress.BucketName,
+				AccessKeyId:     dataAddress.S3StorageDataAddress.AccessKeyId,
+				SecretAccessKey: dataAddress.S3StorageDataAddress.SecretAccessKey,
+			},
+		}, nil
+	}
+	return nil, fmt.Errorf("Unsupported type of asset address")
+}
+
+func createDataAssetFromInput(asset Asset) AssetApiInput {
+	return AssetApiInput{
+		AssetProperties: asset.AssetProperties,
+	}
+}
+
+func validateDataAddressInput(dataAddress DataAddress) error {
+	nonNilFieldsCount := 0
+	v := reflect.ValueOf(dataAddress)
+	for i := 0; i < v.NumField(); i++ {
+		if !v.Field(i).IsNil() {
+			nonNilFieldsCount++
+		}
+	}
+	if nonNilFieldsCount > 1 {
+		return fmt.Errorf("data address field is invalid, cannot have more than 1 address property")
+	}
+	return nil
+}
---
+++ go/service/assets/api_op_CreateAsset_test.go
@@ -0,0 +1,272 @@
+package assets
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/Think-iT-Labs/edc-connector-client/go/edc"
+	edchttp "github.com/Think-iT-Labs/edc-connector-client/go/edc/transport/http"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateAsset(t *testing.T) {
+	authToken := "dummy"
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		payload, err := ioutil.ReadAll(r.Body)
+		assert.NoError(t, err, "error while reading request body")
+		assert.JSONEq(t, `
+{
+	"asset": {
+		"properties": {
+			"asset:prop:id": "1234",
+			"asset:prop:name": "product description",
+			"asset:prop:contenttype": "application/json"
+		}
+	},
+	"dataAddress": {
+		"properties": {
+			"name": "Test asset",
+			"baseUrl": "https://jsonplaceholder.typicode.com/users",
+			"type": "HttpData"
+		}
+	}
+}`, string(payload), "invalid payload")
+
+		fmt.Fprintf(w, `
+{
+	"createdAt": 1680004526,
+	"id": "1234"
+}		
+`)
+	}))
+	defer svr.Close()
+
+	cfg := edc.NewConfig()
+
+	cfg.Addresses = edc.Addresses{
+		Management: &svr.URL,
+	}
+	httpClient, err := edchttp.NewHTTPClient(&authToken)
+	assert.NoError(t, err, "failed to initialize Http Client")
+	cfg.HTTPClient = httpClient
+
+	apiClient, err := New(*cfg)
+	assert.NoError(t, err, "failed to initialize api client")
+
+	assetName := "Test asset"
+	assetBaseUrl := "https://jsonplaceholder.typicode.com/users"
+	assetId := "1234"
+	createAssetOutput, err := apiClient.CreateAsset(
+		CreateAssetInput{
+			Asset{
+				AssetProperties: map[string]string{
+					"asset:prop:id":          assetId,
+					"asset:prop:name":        "product description",
+					"asset:prop:contenttype": "application/json",
+				},
+			},
+			DataAddress{
+				HttpDataAddress: &HttpDataAddress{
+					BaseAddress: &BaseAddress{
+						Type: "HttpData",
+					},
+					Name:    &assetName,
+					BaseUrl: &assetBaseUrl,
+				},
+			},
+		},
+	)
+	assert.NoError(t, err, "failed to create asset.")
+	assert.Equal(t, createAssetOutput.Id, assetId)
+}
+
+func Test_validateDataAddressInput(t *testing.T) {
+	httpName := "http"
+	type args struct {
+		dataAddress DataAddress
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "valid input",
+			args: args{
+				dataAddress: DataAddress{
+					HttpDataAddress: &HttpDataAddress{Name: &httpName},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid input",
+			args: args{
+				dataAddress: DataAddress{
+					HttpDataAddress:      &HttpDataAddress{Name: &httpName},
+					S3StorageDataAddress: &S3StorageDataAddress{Name: &httpName},
+				},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := validateDataAddressInput(tt.args.dataAddress); (err != nil) != tt.wantErr {
+				t.Errorf("validateDataAddressInput() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_createDataAddressFromInput(t *testing.T) {
+	httpName := "http"
+	httpPath := "path"
+	httpMethod := "method"
+	httpBaseUrl := "baseUrl"
+	httpAuthKey := "authKey"
+	httpAuthCode := "authCode"
+	httpSecretName := "secretName"
+	httpProxyBody := "proxyBody"
+	httpProxyPath := "proxyPath"
+	httpProxyQueryParams := "proxyQueryParams"
+	httpProxyMethod := "secretName"
+	httpContentType := "contentType"
+
+	s3Name := "name"
+	s3BucketName := "bucketName"
+	s3AccessKeyId := "accessKeyId"
+	s3SecretAccessKey := "secretAccessKey"
+
+	azureContainer := "container"
+	azureBlobName := "blobname"
+	azureAccount := "accountName"
+
+	type args struct {
+		dataAddress DataAddress
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *DataAddressApiInput
+		wantErr bool
+	}{
+		{
+			name:    "Http data address property should be passed correctly",
+			wantErr: false,
+			want: &DataAddressApiInput{
+				DataProperties: HttpData{
+					Type:             "HttpData",
+					Name:             &httpName,
+					Path:             &httpPath,
+					Method:           &httpMethod,
+					BaseUrl:          &httpBaseUrl,
+					AuthKey:          &httpAuthKey,
+					AuthCode:         &httpAuthCode,
+					SecretName:       &httpSecretName,
+					ProxyBody:        &httpProxyBody,
+					ProxyPath:        &httpProxyPath,
+					ProxyQueryParams: &httpProxyQueryParams,
+					ProxyMethod:      &httpProxyMethod,
+					ContentType:      &httpContentType,
+				},
+			},
+			args: args{
+				dataAddress: DataAddress{
+					HttpDataAddress: &HttpDataAddress{
+						BaseAddress: &BaseAddress{
+							Type: "HttpData",
+						},
+						Name:             &httpName,
+						Path:             &httpPath,
+						Method:           &httpMethod,
+						BaseUrl:          &httpBaseUrl,
+						AuthKey:          &httpAuthKey,
+						AuthCode:         &httpAuthCode,
+						SecretName:       &httpSecretName,
+						ProxyBody:        &httpProxyBody,
+						ProxyPath:        &httpProxyPath,
+						ProxyQueryParams: &httpProxyQueryParams,
+						ProxyMethod:      &httpProxyMethod,
+						ContentType:      &httpContentType,
+					},
+				},
+			},
+		},
+		{
+			name:    "S3 data address property should be passed correctly",
+			wantErr: false,
+			want: &DataAddressApiInput{
+				DataProperties: S3Data{
+					Type:            "AmazonS3",
+					Name:            &s3Name,
+					BucketName:      &s3BucketName,
+					AccessKeyId:     &s3AccessKeyId,
+					SecretAccessKey: &s3SecretAccessKey,
+				},
+			},
+			args: args{
+				dataAddress: DataAddress{
+					S3StorageDataAddress: &S3StorageDataAddress{
+						BaseAddress: &BaseAddress{
+							Type: "AmazonS3",
+						},
+						Name:            &s3Name,
+						BucketName:      &s3BucketName,
+						AccessKeyId:     &s3AccessKeyId,
+						SecretAccessKey: &s3SecretAccessKey,
+					},
+				},
+			},
+		},
+		{
+			name:    "Azure data address property should be passed correctly",
+			wantErr: false,
+			want: &DataAddressApiInput{
+				DataProperties: AzureData{
+					Type:      "AzureStorage",
+					Account:   &azureAccount,
+					BlobName:  &azureBlobName,
+					Container: &azureContainer,
+				},
+			},
+			args: args{
+				dataAddress: DataAddress{
+					AzureStorageDataAddress: &AzureStorageDataAddress{
+						BaseAddress: &BaseAddress{
+							Type: "AzureStorage",
+						},
+						Account:   &azureAccount,
+						BlobName:  &azureBlobName,
+						Container: &azureContainer,
+					},
+				},
+			},
+		},
+		{
+			name: "Unkown data address property is passed",
+			args: args{
+				dataAddress: DataAddress{},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := createDataAddressFromInput(tt.args.dataAddress)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("createDataAddressFromInput() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("createDataAddressFromInput() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
