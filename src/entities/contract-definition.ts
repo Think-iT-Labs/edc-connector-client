6c773166aa50e92ecd1fa9978f8fa2240088ffcc
Olfa <61057075+OlfaBensoussia@users.noreply.github.com>
2023-05-16 11:23:45 +0100

    fix: update contract definition object with missing properties (#33)

--- src/entities/contract-definition.ts
+++ src/entities/contract-definition.ts
@@ -1,10 +1,12 @@
 import { Criterion } from "./criterion";
 
-export interface ContractDefinition {
+export interface ContractDefinitionInput {
   id: string;
   accessPolicyId: string;
   contractPolicyId: string;
   criteria: Criterion[];
 }
-
-export type ContractDefinitionInput = ContractDefinition;
+export interface ContractDefinition extends ContractDefinitionInput {
+  createdAt: number;
+  validity?: number;
+}
