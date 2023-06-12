826cc8af8b1c88d33a8c2bd815c3847a2344ab29
AhmedGrati <ahmedgrati1999@gmail.com>
2023-06-12 15:25:31 +0100

    feat: refactor get asset api call
    Signed-off-by: AhmedGrati <ahmedgrati1999@gmail.com>

--- go/service/assets/api_op_GetAsset.go
+++ go/service/assets/api_op_GetAsset.go
@@ -11,34 +11,19 @@ import (
 
 func (c *Client) GetAsset(assetId string) (*AssetOutput, error) {
 	endpoint := fmt.Sprintf("%s/assets/%s", *c.Addresses.Management, assetId)
-	asset := AssetOutput{}
+	asset := &AssetOutput{}
 
-	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
+	err := c.invokeOperation(internal.InvokeHTTPOperationOptions{
+		Method:             http.MethodGet,
+		Endpoint:           endpoint,
+		ResponsePayload:    asset,
+		ExpectedStatusCode: http.StatusOK,
+	})
 	if err != nil {
-		return nil, sdkErrors.FromError(err).FailedTo(internal.ACTION_HTTP_BUILD_REQUEST)
+		return nil, err
 	}
 
-	res, err := c.HTTPClient.Do(req)
-	if err != nil {
-		return nil, sdkErrors.FromError(err).FailedTo(internal.ACTION_HTTP_DO_REQUEST)
-	}
-
-	defer res.Body.Close()
-	response, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, sdkErrors.FromError(err).FailedTo(internal.ACTION_HTTP_READ_BYTES)
-	}
-
-	if res.StatusCode != http.StatusOK {
-		return nil, sdkErrors.FromError(internal.ParseConnectorApiError(response)).Error(internal.ERROR_API_ERROR)
-	}
-
-	err = json.Unmarshal(response, &asset)
-	if err != nil {
-		return nil, sdkErrors.FromError(err).FailedTof(internal.ACTION_JSON_UNMARSHAL, response)
-	}
-
-	return &asset, err
+	return asset, nil
 }
 
 func (c *Client) GetAssetDataAddress(assetId string) (*AssetDataAddressOutput, error) {
