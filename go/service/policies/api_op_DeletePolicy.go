22420b0895d73004e4cac5d2846026b6bf8db62a
AhmedGrati <ahmedgrati1999@gmail.com>
2023-06-12 16:49:10 +0100

    feat: refactor delete policy
    Signed-off-by: AhmedGrati <ahmedgrati1999@gmail.com>

--- go/service/policies/api_op_DeletePolicy.go
+++ go/service/policies/api_op_DeletePolicy.go
@@ -2,7 +2,6 @@ package policies
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 
 	"github.com/Think-iT-Labs/edc-connector-client/go/internal"
@@ -11,24 +10,9 @@ import (
 func (c *Client) DeletePolicy(policyId string) error {
 	endpoint := fmt.Sprintf("%s/policydefinitions/%s", *c.Addresses.Management, policyId)
 
-	req, err := http.NewRequest(http.MethodDelete, endpoint, nil)
-	if err != nil {
-		return sdkErrors.FromError(err).FailedTo(internal.ACTION_HTTP_BUILD_REQUEST)
-	}
-
-	res, err := c.HTTPClient.Do(req)
-	if err != nil {
-		return sdkErrors.FromError(err).FailedTo(internal.ACTION_HTTP_DO_REQUEST)
-	}
-
-	defer res.Body.Close()
-	response, err := io.ReadAll(res.Body)
-	if err != nil {
-		return sdkErrors.FromError(err).FailedTo(internal.ACTION_HTTP_READ_BYTES)
-	}
-
-	if res.StatusCode != http.StatusNoContent {
-		return sdkErrors.FromError(internal.ParseConnectorApiError(response)).Error(internal.ERROR_API_ERROR)
-	}
-	return nil
+	return c.invokeOperation(internal.InvokeHTTPOperationOptions{
+		Method:             http.MethodDelete,
+		Endpoint:           endpoint,
+		ExpectedStatusCode: http.StatusNoContent,
+	})
 }
