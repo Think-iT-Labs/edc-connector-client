f3846b32fff4e15e5ba499d5909b94b7bdfe97ff
AhmedGrati <ahmedgrati1999@gmail.com>
2023-06-12 17:32:03 +0100

    feat: refactor get cd
    Signed-off-by: AhmedGrati <ahmedgrati1999@gmail.com>

--- go/service/contractdefinition/api_op_GetContractDefinition.go
+++ go/service/contractdefinition/api_op_GetContractDefinition.go
@@ -1,9 +1,7 @@
 package contractdefinition
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 
 	"github.com/Think-iT-Labs/edc-connector-client/go/internal"
@@ -11,32 +9,15 @@ import (
 
 func (c *Client) GetContractDefinition(ContractDefinitionId string) (*ContractDefinition, error) {
 	endpoint := fmt.Sprintf("%s/contractdefinitions/%s", *c.Addresses.Management, ContractDefinitionId)
-	contractDefinition := ContractDefinition{}
+	contractDefinition := &ContractDefinition{}
 
-	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
-	if err != nil {
-		return nil, sdkErrors.FromError(err).FailedTo(internal.ACTION_HTTP_BUILD_REQUEST)
+	if err := c.invokeOperation(internal.InvokeHTTPOperationOptions{
+		Endpoint:           endpoint,
+		Method:             http.MethodGet,
+		ResponsePayload:    contractDefinition,
+		ExpectedStatusCode: http.StatusOK,
+	}); err != nil {
+		return nil, err
 	}
-
-	res, err := c.HTTPClient.Do(req)
-	if err != nil {
-		return nil, sdkErrors.FromError(err).FailedTo(internal.ACTION_HTTP_DO_REQUEST)
-	}
-	defer res.Body.Close()
-
-	response, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, sdkErrors.FromError(err).FailedTo(internal.ACTION_HTTP_READ_BYTES)
-	}
-
-	if res.StatusCode != http.StatusOK {
-		return nil, sdkErrors.FromError(internal.ParseConnectorApiError(response)).Error(internal.ERROR_API_ERROR)
-	}
-
-	err = json.Unmarshal(response, &contractDefinition)
-	if err != nil {
-		return nil, sdkErrors.FromError(err).FailedTof(internal.ACTION_JSON_UNMARSHAL, response)
-	}
-
-	return &contractDefinition, nil
+	return contractDefinition, nil
 }
