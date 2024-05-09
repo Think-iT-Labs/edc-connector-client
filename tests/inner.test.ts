ac09e5f44ae14454e555a2c35d4f606167ce683b
ndr_brt <andrea.bertagnolli@gmail.com>
2024-05-09 14:56:32 +0200

    refactor: switch asset tests to prism (#221)

---
+++ tests/inner.test.ts
@@ -0,0 +1,46 @@
+import nock = require('nock');
+import { EdcConnectorClientError, EdcConnectorClientErrorType } from "../src";
+import { Inner } from "../src/inner";
+
+describe("Inner", () => {
+  const url = "http://www.example.com";
+  const inner = new Inner();
+
+  it("should map error 404", async () => {
+    nock(url).post('/path').reply(404);
+
+    const response = inner.request(url, {
+      path: "/path",
+      method: "POST"
+    })
+
+    await expect(response).rejects.toThrowError("resource not found");
+
+    response.catch((error) => {
+      expect(error).toBeInstanceOf(EdcConnectorClientError);
+      expect(error as EdcConnectorClientError).toHaveProperty(
+        "type",
+        EdcConnectorClientErrorType.NotFound,
+      );
+    });
+  })
+
+  it("should map error 409", async () => {
+    nock(url).post('/path').reply(409);
+
+    const response = inner.request(url, {
+      path: "/path",
+      method: "POST"
+    })
+
+    await expect(response).rejects.toThrowError("duplicated resource");
+
+    response.catch((error) => {
+      expect(error).toBeInstanceOf(EdcConnectorClientError);
+      expect(error as EdcConnectorClientError).toHaveProperty(
+        "type",
+        EdcConnectorClientErrorType.Duplicate,
+      );
+    });
+  })
+})
