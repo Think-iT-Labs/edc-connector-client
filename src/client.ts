913d87c0e08db5fd859a60652f9e6abb923ffaf2
Federico Dionisi <federicodionisi@think-it.io>
2024-07-22 11:26:41 +0200

    feat: improve transpiled typings (#245)

--- src/client.ts
+++ src/client.ts
@@ -11,37 +11,105 @@ export type EdcConnectorClientType<
   T extends Record<string, EdcController>,
 > = EdcConnectorClient & T;
 
+const apiTokenSymbol = Symbol("[#apiToken]");
+const addressesSymbol = Symbol("[#addressesToken]");
+const innerSymbol = Symbol("[#innerToken]");
+
+class Builder<T extends Record<string, EdcController> = {}> {
+  #instance = new EdcConnectorClient();
+
+  apiToken(apiToken: string): this {
+    this.#instance[apiTokenSymbol] = apiToken;
+    return this;
+  }
+
+  managementUrl(managementUrl: string): this {
+    this.#instance[addressesSymbol].management = managementUrl;
+    return this;
+  }
+
+  defaultUrl(defaultUrl: string): this {
+    this.#instance[addressesSymbol].default = defaultUrl;
+    return this;
+  }
+
+  protocolUrl(protocolUrl: string): this {
+    this.#instance[addressesSymbol].protocol = protocolUrl;
+    return this;
+  }
+
+  publicUrl(publicUrl: string): this {
+    this.#instance[addressesSymbol].public = publicUrl;
+    return this;
+  }
+
+  controlUrl(controlUrl: string): this {
+    this.#instance[addressesSymbol].control = controlUrl;
+    return this;
+  }
+
+  use<K extends string, C extends EdcController>(
+    key: K,
+    Controller: Class<C>,
+  ): Builder<T & Record<K, C>> {
+    Object.defineProperty(this.#instance, key, {
+      get() {
+        return new Controller(
+          this.inner,
+          this.createContext(
+            this.apiToken!,
+            this.addresses,
+          ),
+        );
+      },
+      enumerable: true,
+      configurable: false,
+    });
+
+    // SAFETY: we use `Object.defineProperty` above to extend the `EdcConnectorClient` instance.
+    return this as any;
+  }
+
+  build(): EdcConnectorClientType<
+    T
+  > {
+    return this.#instance as
+      & EdcConnectorClient
+      & T;
+  }
+}
+
 export class EdcConnectorClient {
-  #apiToken: string | undefined;
-  #addresses: Addresses = {};
-  #inner = new Inner();
+  [apiTokenSymbol]: string | undefined;
+  [addressesSymbol]: Addresses = {};
+  [innerSymbol] = new Inner();
 
   get management() {
     const context = new EdcConnectorClientContext(
-      this.#apiToken,
-      this.#addresses,
+      this[apiTokenSymbol],
+      this[addressesSymbol],
     );
-    return new ManagementController(this.#inner, context);
+    return new ManagementController(this[innerSymbol], context);
   }
 
   get observability() {
     const context = new EdcConnectorClientContext(
-      this.#apiToken,
-      this.#addresses,
+      this[apiTokenSymbol],
+      this[addressesSymbol],
     );
-    return new ObservabilityController(this.#inner, context);
+    return new ObservabilityController(this[innerSymbol], context);
   }
 
   get public() {
     const context = new EdcConnectorClientContext(
-      this.#apiToken,
-      this.#addresses,
+      this[apiTokenSymbol],
+      this[addressesSymbol],
     );
-    return new PublicController(this.#inner, context);
+    return new PublicController(this[innerSymbol], context);
   }
 
   get addresses() {
-    return this.#addresses;
+    return { ...this[addressesSymbol] };
   }
 
   createContext(
@@ -55,72 +123,5 @@ export class EdcConnectorClient {
     return version;
   }
 
-  static Builder = class Builder<
-    Controllers extends Record<
-      string,
-      EdcController
-    > = {},
-  > {
-    #instance = new EdcConnectorClient();
-
-    apiToken(apiToken: string): this {
-      this.#instance.#apiToken = apiToken;
-      return this;
-    }
-
-    managementUrl(managementUrl: string): this {
-      this.#instance.#addresses.management = managementUrl;
-      return this;
-    }
-
-    defaultUrl(defaultUrl: string): this {
-      this.#instance.#addresses.default = defaultUrl;
-      return this;
-    }
-
-    protocolUrl(protocolUrl: string): this {
-      this.#instance.#addresses.protocol = protocolUrl;
-      return this;
-    }
-
-    publicUrl(publicUrl: string): this {
-      this.#instance.#addresses.public = publicUrl;
-      return this;
-    }
-
-    controlUrl(controlUrl: string): this {
-      this.#instance.#addresses.control = controlUrl;
-      return this;
-    }
-
-    use<K extends string, C extends EdcController>(
-      key: K,
-      Controller: Class<C>,
-    ): Builder<Controllers & Record<K, C>> {
-      Object.defineProperty(this.#instance, key, {
-        get() {
-          return new Controller(
-            this.#inner,
-            this.createContext(
-              this.#apiToken!,
-              this.#addresses,
-            ),
-          );
-        },
-        enumerable: true,
-        configurable: false,
-      });
-
-      // SAFETY: we use `Object.defineProperty` above to extend the `EdcConnectorClient` instance.
-      return this as any;
-    }
-
-    build(): EdcConnectorClientType<
-      Controllers
-    > {
-      return this.#instance as
-        & EdcConnectorClient
-        & Controllers;
-    }
-  };
+  static Builder = Builder;
 }
