7d33f76194ebfacca2f3fffb64bb6426abf5cad6
andrea bertagnolli <andrea.bertagnolli@gmail.com>
2024-09-24 14:16:02 +0200

    build(deps): bump EDC to 0.9.0 (#317)

--- connector/build.gradle.kts
+++ connector/build.gradle.kts
@@ -10,11 +10,12 @@ repositories {
 
 dependencies {
     implementation(libs.edc.runtime.metamodel)
+    implementation(libs.edc.configuration.filesystem)
     implementation(libs.edc.control.api.configuration)
     implementation(libs.edc.control.plane.api.client)
     implementation(libs.edc.control.plane.core)
     implementation(libs.edc.dsp)
-    implementation(libs.edc.configuration.filesystem)
+    implementation(libs.edc.http)
     implementation(libs.edc.iam.mock)
     implementation(libs.edc.management.api)
     implementation(libs.edc.api.observability)
@@ -31,10 +32,11 @@ dependencies {
     implementation(libs.edc.data.plane.selector.core)
 
     implementation(libs.edc.data.plane.self.registration)
-    implementation(libs.edc.data.plane.control.api)
+    implementation(libs.edc.data.plane.signaling.api)
     implementation(libs.edc.data.plane.public.api.v2)
     implementation(libs.edc.data.plane.core)
     implementation(libs.edc.data.plane.http)
+    implementation(libs.edc.data.plane.iam)
 
     implementation(libs.edc.federatedcatalog.core)
     implementation(libs.edc.federatedcatalog.api)
