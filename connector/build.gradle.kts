db28f653611b468dc419a4610e65c58cf8d39835
Mahdy Berriri <44408019+Maa-ddy@users.noreply.github.com>
2025-09-19 15:38:55 +0100

    bump edc to version 0.13.0 (#442)
    * bump edc to version 0.13.0
    adjust version for edc-transfer-pull-http-dynamic-receiver
    implement missing method for TargetNodeDirectory
    
    * revert pull http receiver dependency
    
    * replace individual dependencies with bom's
    
    ---------
    
    Co-authored-by: Mahdi <ctw04415@criticaltechworks.com>

--- connector/build.gradle.kts
+++ connector/build.gradle.kts
@@ -9,38 +9,14 @@ repositories {
 }
 
 dependencies {
-    implementation(libs.edc.runtime.metamodel)
-    implementation(libs.edc.configuration.filesystem)
-    implementation(libs.edc.control.api.configuration)
-    implementation(libs.edc.control.plane.api.client)
-    implementation(libs.edc.control.plane.core)
-    implementation(libs.edc.dsp)
-    implementation(libs.edc.http)
-    implementation(libs.edc.iam.mock)
-    implementation(libs.edc.management.api)
-    implementation(libs.edc.api.observability)
-    implementation(libs.edc.token.core)
-    implementation(libs.edc.transfer.data.plane.signaling)
-    implementation(libs.edc.transfer.pull.http.receiver)
-    implementation(libs.edc.validator.data.address.http.data)
-
-    implementation(libs.edc.edr.cache.api)
-    implementation(libs.edc.edr.store.core)
-    implementation(libs.edc.edr.store.receiver)
-
-    implementation(libs.edc.data.plane.selector.api)
-    implementation(libs.edc.data.plane.selector.control.api)
-    implementation(libs.edc.data.plane.selector.core)
-
-    implementation(libs.edc.data.plane.self.registration)
-    implementation(libs.edc.data.plane.signaling.api)
-    implementation(libs.edc.data.plane.public.api.v2)
-    implementation(libs.edc.data.plane.core)
-    implementation(libs.edc.data.plane.http)
-    implementation(libs.edc.data.plane.iam)
-
-    implementation(libs.edc.federatedcatalog.core)
-    implementation(libs.edc.federatedcatalog.api)
+    runtimeOnly(libs.edc.controlplane.base.bom)
+    runtimeOnly(libs.edc.dataplane.base.bom) {
+        exclude(group = "org.eclipse.edc", module = "data-plane-selector-client")
+    }
+    runtimeOnly(libs.edc.federatedcatalog.bom)
+    implementation(libs.edc.crawler.spi)
+    runtimeOnly(libs.edc.iam.mock)
+    runtimeOnly(libs.edc.dataplane.public.api.v2)
 }
 
 application {
