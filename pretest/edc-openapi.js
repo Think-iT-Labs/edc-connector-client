f72295f0e7160768b4010ca37607b8baff9ca329
Mohamed Dhia <mohameddhia365@gmail.com>
2026-03-12 11:40:52 +0100

    fix: rollback managment api specs to 3.1.4

--- pretest/edc-openapi.js
+++ pretest/edc-openapi.js
@@ -5,27 +5,36 @@ const folder = "node_modules";
 const CONTEXTS = [
   {
     name: "management-api",
-    resourceUrl: "https://eclipse-edc.github.io/Connector/openapi/management-api/management-api.yaml"
+    resourceUrl:
+      "https://eclipse-edc.github.io/Connector/openapi/management-api/3.1.4/management-api.yaml",
   },
   {
     name: "identity-api",
-    resourceUrl: "https://eclipse-edc.github.io/IdentityHub/openapi/identity-api/identity-api.yaml"
+    resourceUrl:
+      "https://eclipse-edc.github.io/IdentityHub/openapi/identity-api/identity-api.yaml",
   },
   {
     name: "presentation-api",
-    resourceUrl: "https://eclipse-edc.github.io/IdentityHub/openapi/presentation-api/presentation-api.yaml"
-  }
-]
+    resourceUrl:
+      "https://eclipse-edc.github.io/IdentityHub/openapi/presentation-api/presentation-api.yaml",
+  },
+];
 
-Promise.all(CONTEXTS.map((context) =>
-  fetch(context.resourceUrl)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Failed to download ${context.name} openapi spec`);
-      }
-      return response.text()
-    })
-    .then(text => fs.writeFile(`${folder}/${context.name}.yml`, text))
-    .catch(error => {
-      console.error(`Error downloading openapi specs for ${context.name}:`, error);
-    })))
+Promise.all(
+  CONTEXTS.map((context) =>
+    fetch(context.resourceUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Failed to download ${context.name} openapi spec`);
+        }
+        return response.text();
+      })
+      .then((text) => fs.writeFile(`${folder}/${context.name}.yml`, text))
+      .catch((error) => {
+        console.error(
+          `Error downloading openapi specs for ${context.name}:`,
+          error,
+        );
+      }),
+  ),
+);
