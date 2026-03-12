e56ae4634da95267699de4d878302875e5237529
chidioguejiofor <chidioguejiofor@gmail.com>
2026-03-12 14:58:16 +0100

    chore: default jsonld loader

--- src/types/jsonld-document-loader.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'jsonld-document-loader' {
-  export class JsonLdDocumentLoader {
-    constructor();
-    addStatic(url: string, document: object): void;
-    build(): (url: string, options?: any) => Promise<import('jsonld').RemoteDocument>;
-  }
-}
