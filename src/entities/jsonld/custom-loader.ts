401ff39e37a2cae2b6ef4ea6b3312d10f1f072bb
chidioguejiofor <chidioguejiofor@gmail.com>
2026-03-13 11:56:36 +0100

    chore: use transmute loader

--- src/entities/jsonld/custom-loader.ts
+++ src/entities/jsonld/custom-loader.ts
@@ -1,4 +1,4 @@
-import jsonld from "jsonld";
+import { documentLoaderFactory } from "@transmute/jsonld-document-loader";
 import dspace2025Data from "./contexts/dspace-2025.json";
 import edcDspaceData from "./contexts/edc-dspace.json";
 import odrlProfileData from "./contexts/odrl-profile.json";
@@ -9,11 +9,7 @@ const JSONLD_CONTEXTS: Record<string, object> = {
   "https://w3id.org/dspace/2025/1/odrl-profile.jsonld": odrlProfileData,
 };
 
-export const documentLoader = async (url: string, options: any) => {
-  const defaultLoader = (jsonld as any).documentLoaders.node();
-  if (JSONLD_CONTEXTS[url]) {
-    const doc = JSONLD_CONTEXTS[url];
-    return { contextUrl: null, documentUrl: url, document: doc };
-  }
-  return defaultLoader(url, options);
-};
+export const documentLoader = documentLoaderFactory
+  .builder()
+  .addContexts(JSONLD_CONTEXTS)
+  .build();
