8523006d8fbf6a3cfa8ddbd023db34381d2c0cd6
andrea bertagnolli <andrea.bertagnolli@gmail.com>
2026-08-06 13:33:22 +0200

    feat: transfer process v4 (#641)

--- tests/controllers/management-tests/transfer-processes.test.ts
+++ tests/controllers/management-tests/transfer-processes.test.ts
@@ -1,4 +1,5 @@
 import {
+  DEFAULT_MANAGEMENT_API_VERSION,
   EdcConnectorClient,
   IdResponse,
   TransferProcessStates,
@@ -6,9 +7,17 @@ import {
 import { createContractAgreement, waitForTransferState } from "../../test-utils";
 
 describe("TransferProcessController", () => {
-  const consumer = new EdcConnectorClient.Builder()
+  const v3Consumer = new EdcConnectorClient.Builder()
     .apiToken("123456")
     .managementUrl("http://localhost:19193/management")
+    .managementApiVersion(DEFAULT_MANAGEMENT_API_VERSION)
+    .protocolUrl("http://consumer-connector:9194/protocol/2025-1")
+    .build();
+
+  const v4Consumer = new EdcConnectorClient.Builder()
+    .apiToken("123456")
+    .managementUrl("http://localhost:19193/management")
+    .managementApiVersion("v4")
     .protocolUrl("http://consumer-connector:9194/protocol/2025-1")
     .build();
 
@@ -18,61 +27,67 @@ describe("TransferProcessController", () => {
     .protocolUrl("http://provider-connector:9194/protocol/2025-1")
     .build();
 
+  const runTransferProcessTests = (
+    label: string,
+    consumer: EdcConnectorClient,
+  ): void => {
+    describe(label, () => {
+      describe("queryAll", () => {
+        it("retrieves all transfer processes", async () => {
+          const idResponse = await initiate(consumer);
+          await waitForTransferState(consumer, idResponse.id, "STARTED");
 
-  describe("queryAll", () => {
-    it("retrieves all tranfer processes", async () => {
-      const idResponse = await initiate();
-      await waitForTransferState(consumer, idResponse.id, "STARTED");
+          const transferProcesses =
+            await consumer.management.transferProcesses.queryAll();
 
-      const transferProcesses =
-        await consumer.management.transferProcesses.queryAll();
+          expect(transferProcesses.length).toBeGreaterThan(0);
+          expect(
+            transferProcesses.find(
+              (transferProcess) => idResponse.id === transferProcess.id,
+            ),
+          ).toBeTruthy();
+        });
+      });
 
-      expect(transferProcesses.length).toBeGreaterThan(0);
-      expect(
-        transferProcesses.find(
-          (transferProcess) => idResponse.id === transferProcess.id,
-        ),
-      ).toBeTruthy();
+      describe("get", () => {
+        it("successfully gets a transfer process", async () => {
+          const idResponse = await initiate(consumer);
+          await waitForTransferState(consumer, idResponse.id, "STARTED");
+
+          const transferProcess =
+            await consumer.management.transferProcesses.get(idResponse.id);
+
+          expect(transferProcess.id).toEqual(idResponse.id);
+        });
+      });
+
+      describe("getState", () => {
+        it("successfully gets a transfer process state", async () => {
+          const idResponse = await initiate(consumer);
+
+          const transferProcessState =
+            await consumer.management.transferProcesses.getState(idResponse.id);
+
+          expect(Object.values(TransferProcessStates)).toContain(
+            transferProcessState.state,
+          );
+        });
+      });
+
+      describe("terminate", () => {
+        it("successfully terminates a transfer process", async () => {
+          const idResponse = await initiate(consumer);
+          await waitForTransferState(consumer, idResponse.id, "STARTED");
+
+          await consumer.management.transferProcesses.terminate(idResponse.id, "a reason");
+
+          await waitForTransferState(consumer, idResponse.id, "TERMINATED");
+        });
+      });
     });
-  });
+  };
 
-  describe("get", () => {
-    it("successfully gets a transfer process", async () => {
-      const idResponse = await initiate();
-      await waitForTransferState(consumer, idResponse.id, "STARTED");
-
-      const transferProcess =
-        await consumer.management.transferProcesses.get(idResponse.id);
-
-      expect(transferProcess.id).toEqual(idResponse.id);
-    });
-  });
-
-  describe("getState", () => {
-    it("successfully gets a transfer process state", async () => {
-      const idResponse = await initiate();
-
-      const transferProcessState =
-        await consumer.management.transferProcesses.getState(idResponse.id);
-
-      expect(Object.values(TransferProcessStates)).toContain(
-        transferProcessState.state,
-      );
-    });
-  });
-
-  describe("terminate", () => {
-    it("successfully terminates a transfer process", async () => {
-      const idResponse = await initiate();
-      await waitForTransferState(consumer, idResponse.id, "STARTED");
-
-      await consumer.management.transferProcesses.terminate(idResponse.id, "a reason");
-
-      await waitForTransferState(consumer, idResponse.id, "TERMINATED");
-    });
-  });
-
-  async function initiate(): Promise<IdResponse> {
+  async function initiate(consumer: EdcConnectorClient): Promise<IdResponse> {
     const { contractAgreement } = await createContractAgreement(
       provider, consumer);
 
@@ -86,4 +101,6 @@ describe("TransferProcessController", () => {
     );
   }
 
+  runTransferProcessTests("v3", v3Consumer);
+  runTransferProcessTests("v4", v4Consumer);
 });
